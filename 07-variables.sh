95fbf07cc2ef661e561732985984dcc761e82e11
nediminenipavan-tech <nediminenipavan@gmail.com>
2025-09-23 22:08:46 +0530

    variables

--- 07-variables.sh
+++ 07-variables.sh
@@ -7,6 +7,6 @@ START_TIME=$(date +%S)
 sleep 10 &
 
 END_TIME=$(date +%S)
-TOTAL-TIME=$($END_TIME-$START_TIME)
+TOTAL-TIME=$(($END_TIME-$START_TIME))
 
 echo "script excuted in: $TOTAL_TIME seconds"
