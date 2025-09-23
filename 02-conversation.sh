44fb3c05e2ba3977475ac1e1a213fa55db518c84
nediminenipavan-tech <nediminenipavan@gmail.com>
2025-09-23 12:31:53 +0530

    Delete 02-conversation.sh

--- 02-conversation.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "ramesh:: hi suresh,how are you?"
-echo "suresh:: hello ramesh,I am fine.how are you?"
-echo "ramesh:: what are you learning recently?"
-echo "suresh:: I am learning shell scripting ramesh"
