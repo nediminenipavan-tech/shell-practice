1ac1b8d8030cd3b6450877b0c99ddb6019d3748f
nediminenipavan-tech <nediminenipavan@gmail.com>
2025-09-23 17:08:54 +0530

    some message

---
+++ 02-coversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash 
+
+echo "ramesh:: hi pavan, how are you?"
+echo  "pavan:: Hello ramesh, i am  fine.how are you?"
+echo  "ramesh:: what are you learning recently?"
+echo  "pavan:: i am learning shell scripting pavan"
