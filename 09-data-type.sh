3959fff25eacc775d87c09689de9c20a48b4c322
nediminenipavan-tech <nediminenipavan@gmail.com>
2025-09-30 22:16:59 +0530

    variables

--- 09-data-type.sh
+++ 09-data-type.sh
@@ -5,6 +5,10 @@ NUMBER1=100
 NUMBER2=200
 NAME=Devops
 
-SUM=$(($NUMBER1+$NUMBER2))
+SUM=$(($NUMBER1+$NUMBER2+$NAME))
 
 echo "SUM is: ${SUM}"
+
+LEADERS=("modi" "putin" "trudo" "trump")
+
+echo "All leaders: ${LEADERS} [@]"
