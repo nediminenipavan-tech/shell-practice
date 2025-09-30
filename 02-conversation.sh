3eac6c6556201358bcc9b5ccd11c3ba40020ff0c
nediminenipavan-tech <nediminenipavan@gmail.com>
2025-09-30 15:55:07 +0530

    some message

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: hi suresh,how are you"
+echo "suresh:: Hello ramesh, I am fine.How are you?"
+echo "ramesh:: what are learing recemtly?"
+echo "suresh:: I am learing shell scripting ramesh"
