9413e409d617fc9fe896979233e2dc674d8dcd1e
nediminenipavan-tech <nediminenipavan@gmail.com>
2025-09-30 20:28:34 +0530

    variables

---
+++ 04-varibles.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+$PERSON1=$1
+$PERSON2=$2
+
+echo "$PERSON1:: welcome to alaska $PERSON2"
+echo "$PERSON2:: Thankyou you $PERSON1, finally not on zoom webinar"
+echo "$PERSON1:: so, do we have a deal"
+echo "$PERSON2:: yes, if you make ukraine joins russia"
