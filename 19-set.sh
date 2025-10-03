6f25c0e2ebbf88c9c7b663cf7b0139728f897ab0
nediminenipavan-tech <nediminenipavan@gmail.com>
2025-10-03 16:57:53 +0530

    session-06

--- 19-set.sh
+++ 19-set.sh
@@ -2,7 +2,12 @@
 
 set -e
 
+error(){
+    echo "there is an error"
+}
+
+trap error ERR
 echo "Hello.."
 echo "Before error.."
-ccdccc;dnf
+ccdccc;dnf #here shell understands there is an error and signal is ERR
 echo "After error"
