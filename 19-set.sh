8369680f85e42a02bbc457aa7e57999fce406df8
nediminenipavan-tech <nediminenipavan@gmail.com>
2025-10-03 17:09:08 +0530

    session-06

--- 19-set.sh
+++ 19-set.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-error(){
-    echo "there is an error $LINENO, coomand is: $BASH_COMMAND"
+# error(){
+ #   echo "there is an error $LINENO, coomand is: $BASH_COMMAND"
 
-}
+# }
 
 trap error ERR
 echo "Hello.."
 echo "Before error.."
-ccdccc;dnf #here shell understands there is an error and signal is ERR
+pavan;dnf # here shell understands there is an error and signal is ERR
 echo "After error"
